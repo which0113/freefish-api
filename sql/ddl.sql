6e1edda4cedf3305433832e07cefdda98bb473a9
which <whichbei@gmail.com>
2024-01-04 00:31:08 +0800

    add 整合dubbo和nacos

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -50,3 +50,15 @@ create table if not exists interface_info (
     updateTime     datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete       tinyint      default 0                 not null comment '是否删除'
 ) comment '接口信息';
+
+-- 用户接口调用表
+create table if not exists user_interface_invoke (
+    id           bigint auto_increment comment 'id' primary key,
+    userId       bigint                             not null comment '调用人id',
+    interfaceId  bigint                             not null comment '接口id',
+    totalInvokes bigint   default 0                 not null comment '总调用次数',
+    status       tinyint  default 0                 not null comment '调用状态（0- 正常 1- 封号）',
+    createTime   datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime   datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint  default 0                 not null comment '是否删除'
+) comment '用户接口调用表';
