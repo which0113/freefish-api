af2179b612948332d3c4c646deec1d6eecaafb52
which <whichbei@gmail.com>
2024-01-09 20:45:26 +0800

    add swap.sh

---
+++ docker/swap.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+# 启动交换空间
+free -m
+fallocate -l 3G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl vm.swappiness=10
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
